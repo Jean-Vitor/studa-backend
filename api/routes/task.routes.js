e95c1c3fe9e53e7c86f44c338ecf9014cb47e5f4
Jean Molino <jean.molino@zallpy.com>
2021-10-14 18:14:50 -0300

    rename middleware login to auth

--- api/routes/task.routes.js
+++ api/routes/task.routes.js
@@ -3,26 +3,26 @@ const {
   create, findAll, findAllCompleted, findOne, update, remove,
 } = require('../controller/task.controller');
 
-const login = require('../middleware/login');
+const auth = require('../middleware/auth');
 
 const router = Router();
 
 // Create a new Task
-router.post('/task', login, create);
+router.post('/task', auth, create);
 
 // Retrieve all Task
-router.get('/task', login, findAll);
+router.get('/task', auth, findAll);
 
 // Retrieve a single Task with id
-router.get('/task/:id', login, findOne);
+router.get('/task/:id', auth, findOne);
 
 // Retrieve a single Task with id
-router.get('/task/completed', login, findAllCompleted);
+router.get('/task/completed', auth, findAllCompleted);
 
 // Update a Task with id
-router.put('/task/:id', login, update);
+router.put('/task/:id', auth, update);
 
 // Delete a Task with id
-router.delete('/task/:id', login, remove);
+router.delete('/task/:id', auth, remove);
 
 module.exports = router;
