ba2ed57c6c4244c38c2ac7c08dc6cdcfd0a60c73
Jean Vitor Oliveira Molino <61634342+Jean-Vitor@users.noreply.github.com>
2021-11-15 13:45:52 -0300

    Config aws

--- start.js
+++ start.js
@@ -6,6 +6,8 @@ const database = require('./api/config/db.config');
 (async () => {
   await database.sync();
   await app.listen(PORT);
+  console.log('Database on')
+  console.log('Listen on port 8000');
 })();
 
 module.exports = app;
